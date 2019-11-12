2e49f1d439f5a858ef0e3dbcca87d8bbc8c44c6a
Kevin <nguyenk215@yahoo.com>
2019-11-11 17:09:27 -0800

    adds art course

--- server/routers/pages.js
+++ server/routers/pages.js
@@ -16,9 +16,14 @@ router.get('/curriculum/course1', function(req, res) {
   res.render('../public/views/courses/course1/course1')
 });
 
-// GET course1
+// GET course2
 router.get('/curriculum/course2', function(req, res) {
   res.render('../public/views/courses/course2/course2')
+});
+
+// GET course3
+router.get('/curriculum/course3', function(req, res) {
+  res.render('../public/views/courses/course3/course3')
 });
 
 module.exports = router;
